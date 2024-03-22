7c9c7de0b60f76072b7b17d63803c646ef4554b7
nitsujz <150884051+nitsujz@users.noreply.github.com>
2024-03-21 23:16:47 -0600

    trying stuff

--- array/sketch.js
+++ array/sketch.js
@@ -1,11 +1,33 @@
 // Arrays and Object Notation
 // Justin Nguyen
 
+let space = 40;
+let radius = space/2;
+let number = 2;
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight, WEBGL);
 }
 
 function draw() {
   background(220);
-  
+
+  orbitControl();
+
+  stroke(0);
+  for (let i=0; i<number; i++) {
+    for (let j=0; j<number; j++) {
+      for (let k=0; k<number; k++) {
+        push();
+        let x = i*space;
+        let y = j*space;
+        let z = k*space;
+        translate(x, y, z);
+        sphere(radius);
+        pop();
+      }
+    }
+  }
+  stroke(255, 0, 0);
+  sphere(radius);
 }
