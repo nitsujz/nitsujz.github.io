b9311b0035516f8707c3614f02fe20e3a9be303d
nitsujz <150884051+nitsujz@users.noreply.github.com>
2024-03-27 14:07:43 -0600

    added slider to array assignment but doesn't work

--- array/sketch.js
+++ array/sketch.js
@@ -1,4 +1,4 @@
-// Arrays and Object Notation
+// Arrays and Object Notation Assignment
 // Justin Nguyen
 
 let theSpheres = [];
@@ -6,24 +6,32 @@ let slider;
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
-  makeSpheres();
   
+  createSlider(100, 30);
+  slider.position(100, 30);
+  slider.size(100);
+
+  makeSpheres();
 }
 
 function draw() {
   background(220);
   showSpheres();
   orbitControl();
+  let a = slider.value();
+  
 }
+
 function makeSpheres() {
   let someSpheres = {
-    radius: random(50, 100),
+    radius: random(40, 70),
     x: width/2,
     y: height/2,
     r: random(255),
     g: random(255),
     b: random(255),
-    
+    alpha: random(255),
+
   };
   theSpheres.push(someSpheres);
 }
