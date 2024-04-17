7f59b22285afd1b65039b92d74a2e3ed14499ae8
nitsujz <150884051+nitsujz@users.noreply.github.com>
2024-04-17 14:11:21 -0600

    make the grid for tetris

--- game of life/sketch.js
+++ game of life/sketch.js
@@ -13,9 +13,14 @@
 
 let grid;
 let cellSize;
-const GRID_SIZE = 30;
+const GRID_SIZE = 40;
 let toggleStyle = "self";
 let isAutoPlayOn = false;
+let gosperGun;
+
+function preload() {
+  gosperGun = loadJSON("gosper.json");
+}
 
 function setup() {
   //make the canvas the largest square that you can...
@@ -62,6 +67,10 @@ function keyPressed() {
 
   if (key === "e") {
     grid = generateEmptyGrid(GRID_SIZE, GRID_SIZE);
+  }
+
+  if (key === "g") {
+    grid = gosperGun.gun;
   }
 
   if (key === "n") {
--- gridassignment/sketch.js
+++ gridassignment/sketch.js
@@ -1,11 +1,43 @@
 // tetris
 // Justin Nguyen
 
+let grid = 
+[[0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0],];
+
+let cellSize;
+const GRID_SIZE = 10;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowWidth);
+
+  noStroke();
+  cellSize = height/grid.length;
 }
 
 function draw() {
-  background(220);
+  background("220");
+
+  displayGrid();
 }
+
+function displayGrid() {
+  for (let y = 0; y < grid.length; y++) {
+    for (let x = 0; x < grid[y].length; x++) {
+      fill("black ");
+      square(x * cellSize, y * cellSize, cellSize);
+    }
+  }
+}
+
