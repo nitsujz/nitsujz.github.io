a8c62a12c086817731d08c00707e4ee979184ba0
nitsujz <150884051+nitsujz@users.noreply.github.com>
2024-04-18 14:11:21 -0600

    make blocks for tetris

--- gridassignment/sketch.js
+++ gridassignment/sketch.js
@@ -1,7 +1,7 @@
 // tetris
 // Justin Nguyen
 
-let grid = 
+const grid = 
 [[0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0],
@@ -16,11 +16,43 @@ let grid =
   [0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0],];
 
+const blocks = {
+  I: [
+    [1, 1, 1, 1]
+  ],
+
+  L: [
+    [1, 0],
+    [1, 0],
+    [1, 1],
+  ],
+
+  O: [
+    [1, 1],
+    [1, 1],
+  ],
+
+  S: [
+    [1, 1, 0],
+    [0, 1, 1],
+  ],
+
+  T: [
+    [1, 1, 1],
+    [0, 1, 0],
+  ],
+
+  Z: [
+    [0, 1, 1],
+    [1, 1, 0],
+  ],
+};
+
 let cellSize;
 const GRID_SIZE = 10;
 
 function setup() {
-  createCanvas(windowWidth, windowWidth);
+  createCanvas(windowWidth, windowHeight);
 
   noStroke();
   cellSize = height/grid.length;
@@ -30,14 +62,19 @@ function draw() {
   background("220");
 
   displayGrid();
+  blocksFall();
 }
 
 function displayGrid() {
   for (let y = 0; y < grid.length; y++) {
     for (let x = 0; x < grid[y].length; x++) {
-      fill("black ");
+      fill("black");
       square(x * cellSize, y * cellSize, cellSize);
     }
   }
 }
 
+function blocksFall() {
+
+}
+ 
